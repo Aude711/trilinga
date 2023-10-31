e149ceb2436b38979cbd90804ae3f1976a97b271
Altyrion69 <altyrion@hotmail.fr>
2023-10-31 14:46:17 +0100

    Routes created

--- src/routes.php
+++ src/routes.php
@@ -15,4 +15,9 @@ return [
     'admin' => ['AdminController', 'index',],
     'admin/add' => ['AdminController', 'add',],
     'admin/edit' => ['AdminController', 'edit', ['id']],
+    'items' => ['ItemController', 'index',],
+    'items/edit' => ['ItemController', 'edit', ['id']],
+    'items/show' => ['ItemController', 'show', ['id']],
+    'items/add' => ['ItemController', 'add',],
+    'items/delete' => ['ItemController', 'delete',],
 ];
