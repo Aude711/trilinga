f3b24b0b5667126927213c1be5958960fa33c879
Altyrion69 <altyrion@hotmail.fr>
2023-11-14 17:05:57 +0100

    connection/déconnexion

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,13 +2,40 @@
 
 namespace App\Controller;
 
+use App\Model\AuthManager;
+
 class AuthController extends AbstractController
 {
-     /**
+    /**
      * List level
      */
-    public function index(): string
+
+    public function login(): string
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $data = array_map('trim', $_POST);
+            $data = array_map('htmlentities', $data);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, update and redirection
+            $authManager = new AuthManager();
+            $user = $authManager->selectOneByEmail($data['email']);
+
+            if ($user && password_verify($data['password'], $user['password'])) {
+                $_SESSION['user_id'] = $user['id'];
+                header('Location: /admin');
+                exit();
+            }
+        }
         return $this->twig->render('login.html.twig');
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['user_id']);
+        header('Location: /');
+        exit();
     }
 }
